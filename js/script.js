48c25caafa06ccbb4bcd6c37a18f37bc931c02ed
Piero <piero.onorati@virgilio.it>
2021-05-28 14:31:37 +0200

    start project

---
+++ js/script.js
@@ -0,0 +1,3 @@
+/* Gioco dei dadi: generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+var player = Math.floor(Math.random()*5) + 1;
