1531f9f0785feeb9e4ac977a546dbe5f3c0139e7
Piero <piero.onorati@virgilio.it>
2021-05-28 16:34:09 +0200

    add algorithm for bonus.js

--- js/bonus.js
+++ js/bonus.js
@@ -1,1 +1,7 @@
-/* Stampare nella console tutti i numero da 10 a 30 (compresi) che sono multipli di 3 */
+/* Stampare nella console tutti i numeri da 10 a 30 (compresi) che sono multipli di 3 */
+
+for ( var i = 10; i<= 30; i++) {
+    if (i ==10 || i % 3 == 0) {
+        console.log(i);
+    }  
+}
