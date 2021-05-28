c02cf97303986b5a872a0cd04c9070419cae2e9f
Piero <piero.onorati@virgilio.it>
2021-05-28 14:43:59 +0200

    create algorithm

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 /* Gioco dei dadi: generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
-var player = Math.floor(Math.random()*5) + 1;
+var player = Math.floor(Math.random()*6) + 1;
+console.log(player);
+
+var computer = Math.floor(Math.random()*6) + 1;
+console.log(computer);
+
+var result = 'player wins';
+
+if (player < computer) {
+    result = 'computers wins';    
+} else if (player == computer) {
+    result = 'both win';
+} 
+
+console.log(result);
